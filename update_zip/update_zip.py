c723c5f6ef4711c57a1bc7b3f56ea68921f37e1b
Lucioric2000 <lucioric2000@hotmail.com>
2023-11-07 23:29:26 -0600

    Working addon

---
+++ update_zip/update_zip.py
@@ -0,0 +1,3 @@
+import zipfile, os
+#python3 -mzipfile -c ../repo/plugin.video.lo-scienziato-pazzo.zip ../plugin.video.lo-scienziato-pazzo
+os.system("python3 -mzipfile -c ../repo/plugin.video.lo-scienziato-pazzo.zip ../plugin.video.lo-scienziato-pazzo")
